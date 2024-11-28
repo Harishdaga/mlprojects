36969917624a7180fc2eb5245b1f265033470773
HarishDaga <mrharishdaga@gmail.com>
2024-11-28 20:00:26 +0530

    few changes

--- application.py
+++ application.py
@@ -23,11 +23,11 @@ def predict_datapoint():
         data=CustomData(
             gender=request.form.get('gender'),
             race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
+            parental_level_of_education=request.form.get('parental level of education'),
             lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
+            test_preparation_course=request.form.get('test preparation course'),
+            reading_score=float(request.form.get('writing score')),
+            writing_score=float(request.form.get('reading score'))
 
         )
         pred_df=data.get_data_as_data_frame()
@@ -42,4 +42,4 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", debug=True)        
+    app.run(host="0.0.0.0")        
